14be61859ac28b9bddc2738f576e68e15919d87b
Lisboa-Hub <biel.lisboa123@hotmail.com>
2024-08-02 17:20:02 -0300

    Versão 1

---
+++ lista_estados.py
@@ -0,0 +1,8 @@
+# Entrada de dados
+Estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins", "Distrito Federal"]
+
+# saída de dados
+print(f"Quantidade de Estados: {len(Estados)}.\n")
+
+for Estado in Estados:
+    print(Estado)
